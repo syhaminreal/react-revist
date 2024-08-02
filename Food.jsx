7745de6b71d761bff126c7ba9d7dd6ccfe247482
Reed Monkey <shyaminreallife@gmail.com>
2024-08-02 10:08:39 +0545

    Add files via upload

---
+++ Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+
+    const food1 = "orange";
+    const food2= "mango";
+    return (
+      <>
+       <li>Apple</li>
+       <li>{food1}</li>
+       <li>{food2.toUpperCase()}</li>
+        <hr />
+      </>
+    );
+  }
+  
+  export default Food;
+  
---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer>
+        <h1>&copy; {new Date().getFullYear()}Copyright by Reed</h1>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
